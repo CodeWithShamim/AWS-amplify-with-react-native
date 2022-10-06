68a3f9af7e82e506c77865ca93c4b1427259eae7
codewithshamim <shamimislamonline@gmail.com>
2022-10-06 11:39:57 +0600

    d

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,6 +8,8 @@ const HomeScreen=(props) => {
             <StatusBar barStyle="dark-content" />
             <SafeAreaView>
                 <Text>Home544 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque animi rerum quam fugiat vel, totam eveniet quisquam, dignissimos at delectus, quis quod obcaecati ducimus dolor enim voluptatem magni necessitatibus vitae.</Text>
+                <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque perferendis, aspernatur eos magnam fugiat beatae ipsa nemo labore eum! Neque suscipit ut unde maxime nam enim labore voluptatibus quisquam asperiores.</Text>
+                <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi explicabo, ipsa quos et magni laudantium ullam voluptate architecto, eligendi quia eveniet ab deserunt tempore eos, sed libero natus illum minima.</Text>
             </SafeAreaView>
         </>
     );
